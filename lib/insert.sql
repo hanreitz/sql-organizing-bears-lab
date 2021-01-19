3cf514068346910e02f91ed898c60dda13906c55
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-01-18 19:10:48 -0500

    commit tree 78945eb5d67e5c4f6ffcd1ec67317d39e5ff76f0

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, 'M', 'brown', 'chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, 'M', 'tan', 'rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, 'F', 'dark brown', 'loving', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, 'M', 'brown', 'commanding', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, 'F', 'tan', 'chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, 'M', 'greenish brown', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, 'F', 'brown', 'motherly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'M', 'brown', 'aggressive', 1);
